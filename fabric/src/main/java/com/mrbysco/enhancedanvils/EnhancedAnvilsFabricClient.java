07add1c66c31f55879d2a4bbcc361a1e87c20e66
Mrbysco <Mrbysco@users.noreply.github.com>
2026-02-04 17:50:23 +0100

    Fix fabric issue

--- fabric/src/main/java/com/mrbysco/enhancedanvils/EnhancedAnvilsFabricClient.java
+++ fabric/src/main/java/com/mrbysco/enhancedanvils/EnhancedAnvilsFabricClient.java
@@ -1,18 +1,25 @@
 package com.mrbysco.enhancedanvils;
 
+import com.mrbysco.enhancedanvils.client.screen.ExtendedAnvilScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.client.gui.screens.inventory.AnvilScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.inventory.AnvilMenu;
 
 public class EnhancedAnvilsFabricClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-//		ScreenEvents.BEFORE_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
-//			if (screen.getClass().getName().equals("net.minecraft.client.gui.screens.inventory.AnvilScreen")) {
-//				AnvilScreen anvilScreen = (AnvilScreen) screen;
-//				AnvilMenu menu = anvilScreen.getMenu();
-//				Component title = anvilScreen.getTitle();
-//				screen.removed();
-//				client.setScreen(new ExtendedAnvilScreen(menu, client.player.getInventory(), title));
-//			}
-//		});
+		ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+			System.out.println(screen.getClass().getName());
+			if (screen.getClass().getName().equals("net.minecraft.client.gui.screens.inventory.AnvilScreen") ||
+					screen.getClass().getName().equals("net.minecraft.class_471")) {
+				AnvilScreen anvilScreen = (AnvilScreen) screen;
+				AnvilMenu menu = anvilScreen.getMenu();
+				Component title = anvilScreen.getTitle();
+				screen.removed();
+				client.setScreen(new ExtendedAnvilScreen(menu, client.player.getInventory(), title));
+			}
+		});
 	}
 }

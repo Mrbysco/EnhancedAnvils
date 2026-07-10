735d0564a5800ee500d9e17f98eb00b58b8c7b0a
Mrbysco <Mrbysco@users.noreply.github.com>
2026-07-10 22:16:59 +0200

    Fix classloading (neoforge)

--- neoforge/src/main/java/com/mrbysco/enhancedanvils/client/ClientHandler.java
+++ neoforge/src/main/java/com/mrbysco/enhancedanvils/client/ClientHandler.java
@@ -3,10 +3,15 @@ package com.mrbysco.enhancedanvils.client;
 import com.mrbysco.enhancedanvils.client.screen.ExtendedAnvilScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AnvilScreen;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ScreenEvent;
 
+@EventBusSubscriber(Dist.CLIENT)
 public class ClientHandler {
 
+	@SubscribeEvent
 	public static void onScreenOpen(ScreenEvent.Opening event) {
 		// Check if screen is net.minecraft.client.gui.screens.inventory.AnvilScreen and not an instance of one (Modded Anvil Screens)
 		if (event.getScreen().getClass().getName().equals("net.minecraft.client.gui.screens.inventory.AnvilScreen")) {
